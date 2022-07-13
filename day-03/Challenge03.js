ce3cae2e451ab1e7b9e44fc0cbebc05e78899e30
baraaAlosaily <seelbaraa@gmail.com>
2022-07-13 11:37:17 +0300

    fix challenge one two three four issues

--- day-03/Challenge03.js
+++ day-03/Challenge03.js
@@ -73,7 +73,7 @@ const cvFormatter = (arr) => {
 // From left to right read nums[i] and index[i], insert at index index[i] the
 // value nums[i] in target array.
 // Repeat the previous step until there are no elements to read in nums and index.
-// Input: nums = [0,1,2,3,4], index = [0,1,2,2,1]
+// Input: nums = [0, 1, 2, 3, 4], index = [0, 4, 1, 2, 3]
 // Output: [0,4,1,3,2]
 const targetArrayInGivenOrder = (numsArr, indexArr) => {
   let targetArr;
