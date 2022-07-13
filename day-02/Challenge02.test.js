9e2711c75681764c9c01aaa2823367a243c5ddf5
ahmad-swedani <swedani390@gmail.com>
2022-07-13 09:16:11 +0300

    .

--- day-02/Challenge02.test.js
+++ day-02/Challenge02.test.js
@@ -35,7 +35,7 @@ describe("Challenge01", () => {
     expect(shuffleString("codeleet", [4, 5, 6, 7, 0, 2, 1, 3])).toStrictEqual(
       "leetcode"
     );
-    expect(shuffleString("abc"), [0, 1, 2]).toStrictEqual("abc");
+    expect(shuffleString("abc", [0, 1, 2])).toStrictEqual("abc");
     expect(
       shuffleString("worldhello", [5, 4, 7, 3, 9, 0, 1, 8, 2, 6])
     ).toStrictEqual("helloworld");
